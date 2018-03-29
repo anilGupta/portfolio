b298f0ddb36d050da36e8b64a49c55cbe377aa3e
anilg <anil.gupta@nw18.com>
2018-03-29 20:01:54 +0530

    -- github route issue fixed

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import App from './containers/App';
 
 export default class extends Component {
   render() {
-    return <BrowserRouter>
+    return <BrowserRouter basename={process.env.PUBLIC_URL}>
             <div>
               <Route path="/" component={App} />
             </div>
